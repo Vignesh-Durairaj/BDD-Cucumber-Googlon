a0326773bf53260b5b4c9e01064f65147568f01e
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2018-12-04 15:30:39 +0530

    Modified the test package layout

--- src/test/java/runners/GooglonRunner.java
+++ src/test/java/runners/GooglonRunner.java
@@ -1,4 +1,4 @@
-package com.credijusto.googlon.runners;
+package runners;
 
 import org.junit.runner.RunWith;
 
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="features", glue="src/test/java")
+@CucumberOptions(features="src/test/java/features", glue="src/test/java/stepDefinitions")
 public class GooglonRunner {
 
 }
